09bb62a944b5a980b7814415b3a80a633722ccea
mario <mariodiaz1375@gmail.com>
2025-09-07 18:58:21 -0300

    se modificaron de pacientes, admin, urls y views. para agregar get, put y delete de un paciente

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,14 +5,39 @@ from .serializers import PacientesSerializer
 # Create your views here.
 
 class PacientesList(APIView):
+    # obtiene todos los pacientes listados
     def get(self, request):
         pacientes = Pacientes.objects.all()
         serializer = PacientesSerializer(pacientes, many=True)
         return Response(serializer.data)
     
+    # guarda un paciente en la lista de pacientes
     def post(self, request):
         serializer = PacientesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+class PacienteDetail(APIView):
+    # obtiene un paciente, pasando su id
+    def get(self, request, pk):
+        paciente = Pacientes.objects.get(pk=pk)
+        serializer = PacientesSerializer(paciente)
+        return Response(serializer.data)
+    
+    # puede editar un paciente, pasando su id
+    def put(self, request, pk):
+        paciente = Pacientes.objects.get(pk=pk)
+        serializer = PacientesSerializer(paciente, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    # puede borrar un paciente, pasando su id
+    def delete(self, request, pk):
+        paciente = Pacientes.objects.get(pk=pk)
+        paciente.delete()
+        return Response(status=204)
+    
